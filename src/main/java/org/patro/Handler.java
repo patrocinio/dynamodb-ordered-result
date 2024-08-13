276658bfbd4b490c4804729a9978bf89ab93a44a
Eduardo Patrocinio <epatro@amazon.com>
2024-08-13 10:59:53 -0400

    Move to async interface

--- src/main/java/org/patro/Handler.java
+++ src/main/java/org/patro/Handler.java
@@ -1,6 +1,7 @@
 package org.patro;
 
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
 import software.amazon.awssdk.services.dynamodb.model.ComparisonOperator;
@@ -17,6 +18,8 @@ import software.amazon.awssdk.services.dynamodb.model.ScanResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -36,7 +39,7 @@ import org.slf4j.LoggerFactory;
 public class Handler {
     private static final Logger logger = LoggerFactory.getLogger(App.class);
 
-    public static void putItemInTable(DynamoDbClient ddb,
+    public static void putItemInTable(DynamoDbAsyncClient ddb,
             String tableName,
             String key,
             String keyVal,
@@ -54,9 +57,9 @@ public class Handler {
                 .build();
 
         try {
-            PutItemResponse response = ddb.putItem(request);
+            CompletableFuture<PutItemResponse> response = ddb.putItem(request);
             logger.info(tableName + " was successfully updated. The request id is "
-                    + response.responseMetadata().requestId());
+                + response.get().responseMetadata().requestId());
 
         } catch (ResourceNotFoundException e) {
             System.err.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", tableName);
@@ -65,10 +68,13 @@ public class Handler {
         } catch (DynamoDbException e) {
             System.err.println(e.getMessage());
             System.exit(1);
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 
-    public static QueryResponse getItemFromTable(DynamoDbClient ddb,
+    public static QueryResponse getItemFromTable(DynamoDbAsyncClient ddb,
             String tableName,
             String key,
             String keyVal) 
@@ -91,8 +97,8 @@ public class Handler {
 //        logger.info ("request: " + request.toString());
 
         try {
-            QueryResponse response = ddb.query(request);
-            return response;
+            CompletableFuture<QueryResponse> response = ddb.query(request);
+            return response.get();
 
         } catch (ResourceNotFoundException e) {
             System.err.format("Error: The Amazon DynamoDB table \"%s\" can't be found.\n", tableName);
@@ -101,8 +107,10 @@ public class Handler {
         } catch (DynamoDbException e) {
             System.err.println(e.getMessage());
             System.exit(1);
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
         return null;
 
     }
@@ -111,7 +119,7 @@ public class Handler {
         logger.info("WriteRequest starts seqNo: " + seqNoVal);
 
         Region region = Region.US_EAST_1;
-        DynamoDbClient ddb = DynamoDbClient.builder()
+        DynamoDbAsyncClient ddb = DynamoDbAsyncClient.builder()
                 .region(region)
                 .build();
 
@@ -126,7 +134,7 @@ public class Handler {
         logger.info("ReadRequest starts");
 
         Region region = Region.US_EAST_1;
-        DynamoDbClient ddb = DynamoDbClient.builder()
+        DynamoDbAsyncClient ddb = DynamoDbAsyncClient.builder()
                 .region(region)
                 .build();
 
@@ -159,30 +167,35 @@ public class Handler {
 
     public static void trimTable() {
         Region region = Region.US_EAST_1;
-        DynamoDbClient ddb = DynamoDbClient.builder()
+        DynamoDbAsyncClient ddb = DynamoDbAsyncClient.builder()
                 .region(region)
                 .build();
 
         ScanRequest request = ScanRequest.builder().tableName(Configuration.TABLE_NAME).build();
-        ScanResponse response;
+        CompletableFuture<ScanResponse> response;
 
         
-        do {
+        try {
+            do {
             response = ddb.scan(request);
 //            logger.info ("trimTable response: " + response);
             
-            for (Map<String, AttributeValue> item : response.items()) {
-                Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
-                key.put(Configuration.KEY, item.get(Configuration.KEY));
-                key.put(Configuration.SEQ_NO, item.get(Configuration.SEQ_NO));
-                logger.info ("trimTable deleting item: " + item);
-                DeleteItemRequest deleteRequest = DeleteItemRequest.builder().tableName(Configuration.TABLE_NAME).key(key).build();
-                ddb.deleteItem(deleteRequest);
+                for (Map<String, AttributeValue> item : response.get().items()) {
+                    Map<String, AttributeValue> key = new HashMap<String, AttributeValue>();
+                    key.put(Configuration.KEY, item.get(Configuration.KEY));
+                    key.put(Configuration.SEQ_NO, item.get(Configuration.SEQ_NO));
+                    logger.info ("trimTable deleting item: " + item);
+                    DeleteItemRequest deleteRequest = DeleteItemRequest.builder().tableName(Configuration.TABLE_NAME).key(key).build();
+                    ddb.deleteItem(deleteRequest);
 
-            }
+                }
 
-        } while (response.count() > 0);
-         
+
+            } while (response.get().count() > 0);
+        } catch (InterruptedException | ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         ddb.close();
 
 
